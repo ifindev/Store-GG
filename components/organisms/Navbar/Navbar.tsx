3f45166d5ead58d731919754118086628db72f4b
Muhammad Arifin <muhammad.arifin1@ibm.com>
2021-12-31 23:51:43 +0700

    Rewrite long classnames in Navbar to constant

--- components/organisms/Navbar/Navbar.tsx
+++ components/organisms/Navbar/Navbar.tsx
@@ -4,9 +4,10 @@ import NavToggleMenu from './NavToggleMenu'
 import NavIcon from './NavIcon'
 
 const Navbar = () => {
+  const navClassname = 'navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50'
   return (
     <section>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
+      <nav className={navClassname}>
         <div className="container-fluid">
           <NavIcon />
           <NavToggleMenu />
