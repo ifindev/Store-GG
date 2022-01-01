1638bfd505a4ba2943106c606d2fd45ba1891c1e
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-01-01 15:28:12 +0700

    Changed a-tag to Link in GameItem

--- components/molecules/GameItem/GameItem.tsx
+++ components/molecules/GameItem/GameItem.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface GameItemProps {
   title: string
@@ -15,28 +16,30 @@ const GameItem = (props: GameItemProps) => {
   const { title, category, thumbnail } = props
   return (
     <div className="featured-game-card position-relative">
-      <a href="./src/detail.html">
-        <div className="blur-sharp">
-          <Image
-            className="thumbnail"
-            src={`/img/${thumbnail}.png`}
-            width={205}
-            height={270}
-            alt="thumbnail"
-          />
-        </div>
-        <div className="cover position-absolute bottom-0 m-32">
-          <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
-            <div className="game-icon mx-auto">
-              <Image src="/icon/game.svg" width={54} height={36} />
-            </div>
-            <div>
-              <p className="fw-semibold text-white text-xl m-0">{title}</p>
-              <p className="fw-light text-white m-0">{category}</p>
+      <Link href="/detail">
+        <a>
+          <div className="blur-sharp">
+            <Image
+              className="thumbnail"
+              src={`/img/${thumbnail}.png`}
+              width={205}
+              height={270}
+              alt="thumbnail"
+            />
+          </div>
+          <div className="cover position-absolute bottom-0 m-32">
+            <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
+              <div className="game-icon mx-auto">
+                <Image src="/icon/game.svg" width={54} height={36} />
+              </div>
+              <div>
+                <p className="fw-semibold text-white text-xl m-0">{title}</p>
+                <p className="fw-light text-white m-0">{category}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </a>
+        </a>
+      </Link>
     </div>
   )
 }
